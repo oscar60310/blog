73b1b83e23a84d282a2d717a1498a330928965eb
Ivan Tsai <oscar60310@gmail.com>
2020-01-21 00:45:58 +0800

    Performance improve (#4)
    * change geo pattern to async load
    
    * remove unuse library
    
    * css order
    
    * preload css
    
    * reopen comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,10 @@ function toggleMenu() {
 
 jQuery(function() {
   // 回到顶部
-  function toTop () {
+  function toTop() {
     var $toTop = $(".gotop");
 
-    $(window).on("scroll", function () {
+    $(window).on("scroll", function() {
       if ($(window).scrollTop() >= $(window).height()) {
         $toTop.css("display", "block").fadeIn();
       } else {
@@ -20,15 +20,46 @@ jQuery(function() {
       }
     });
 
-    $toTop.on("click", function (evt) {
+    $toTop.on("click", function(evt) {
       var $obj = $("body,html");
-      $obj.animate({
-        scrollTop: 0
-      }, 240);
+      $obj.animate(
+        {
+          scrollTop: 0
+        },
+        240
+      );
 
       evt.preventDefault();
     });
   }
+  function adjust_search_box_width() {
+    if ($(".post-directory").length) {
+      if ($(".post-directory").is(":visible")) {
+        $("#site_search").width(300);
+      }
+    }
+    var searchbar_width = $("#site_search").width();
+    $("#search_box").width(searchbar_width - 65);
+  }
 
+  $(window).on("resize", function() {
+    adjust_search_box_width();
+  });
   toTop();
+  adjust_search_box_width();
 });
+
+function loadGeopattern() {
+  jQuery(document).ready(function($) {
+    // geopattern
+    $(".geopattern").each(function() {
+      $(this).geopattern($(this).data("pattern-id"));
+    });
+    // hljs.initHighlightingOnLoad();
+  });
+}
+
+function loadLozad() {
+  const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+  observer.observe();
+}
