af67987b5af8f9e631016198ccd6c08fafb0e305
Ivan Tsai <oscar60310@gmail.com>
2020-01-21 00:07:14 +0800

    css order

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,21 @@ jQuery(function() {
       evt.preventDefault();
     });
   }
+  function adjust_search_box_width() {
+    if ($(".post-directory").length) {
+      if ($(".post-directory").is(":visible")) {
+        $("#site_search").width(300);
+      }
+    }
+    var searchbar_width = $("#site_search").width();
+    $("#search_box").width(searchbar_width - 65);
+  }
 
+  $(window).on("resize", function() {
+    adjust_search_box_width();
+  });
   toTop();
+  adjust_search_box_width();
 });
 
 function loadGeopattern() {
