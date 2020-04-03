060a7d5d19a66c775025ba1bb5ca8c8db230d23d
Ivan Tsai <oscar60310@gmail.com>
2020-04-03 16:36:43 +0800

    add post info

--- themes/goofy/source/js/post-run.js
+++ themes/goofy/source/js/post-run.js
@@ -1,1 +1,9 @@
-console.log(123);
+const geoPattern = () => {
+  const doms = document.querySelectorAll(".geo-pattern");
+  doms.forEach(dom => {
+    const pattern = GeoPattern.generate(dom.getAttribute("data-pattern"));
+    dom.style["background-image"] = pattern.toDataUrl();
+  });
+};
+
+geoPattern();
