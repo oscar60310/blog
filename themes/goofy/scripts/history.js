d8e2e290518cbf5fe73fb52d2f7c4877126ff409
Ivan Tsai <oscar60310@gmail.com>
2020-04-06 23:58:00 +0800

    fix pre

--- themes/goofy/scripts/history.js
+++ themes/goofy/scripts/history.js
@@ -2,20 +2,22 @@ const path = require("path");
 const exec = require("child_process").exec;
 const moment = require("moment");
 
-const getHistory = result => {
-  return result.split("\n").map(row => {
+const getHistory = (result) => {
+  return result.split("\n").map((row) => {
     const data = row.split(",");
     const message = data.slice(2, data.length).join(",");
     return {
       hash: data[0],
-      time: moment(data[1], "YYYY-MM-DD HH:mm:ss Z").utcOffset('+08:00').format("YYYY/MM/DD HH:mm"),
-      message
+      time: moment(data[1], "YYYY-MM-DD HH:mm:ss Z")
+        .utcOffset("+08:00")
+        .format("YYYY/MM/DD HH:mm"),
+      message,
     };
   });
 };
 
-hexo.extend.filter.register("before_post_render", function(data) {
-  return new Promise(resolve => {
+hexo.extend.filter.register("before_post_render", function (data) {
+  return new Promise((resolve) => {
     const filePath = path.join(
       process.cwd(),
       "source",
@@ -23,7 +25,7 @@ hexo.extend.filter.register("before_post_render", function(data) {
       `${data.slug}.md`
     );
     exec(
-      `git log --pretty=format:'%H,%ad,%s' --date=iso --max-count=10 ${filePath}`,
+      `git log --pretty=format:"%H,%ad,%s" --date=iso --max-count=10 ${filePath}`,
       (error, out) => {
         if (!error) data.history = getHistory(out);
         resolve(data);
