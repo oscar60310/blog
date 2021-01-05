806c88bf92394e184b7a042f828d22a50adfe76c
Ivan Tsai <oscar60310@gmail.com>
2021-01-06 00:34:59 +0800

    add posts.json for id index

--- themes/goofy/scripts/post-id.js
+++ themes/goofy/scripts/post-id.js
@@ -1,6 +1,20 @@
 const crypto = require("crypto");
 
+const getHash = (path) =>
+  crypto.createHash("sha256").update(path).digest("hex");
+
 hexo.extend.filter.register("before_post_render", function (data) {
-  data.identity = crypto.createHash("sha256").update(data.path).digest("hex");
+  data.identity = getHash(data.path);
   return data;
+});
+
+hexo.extend.generator.register("post_id", function (locals) {
+  return {
+    path: "posts.json",
+    data: locals.posts.map((post) => ({
+      id: getHash(post.path),
+      path: post.path,
+      title: post.title,
+    })),
+  };
 });
