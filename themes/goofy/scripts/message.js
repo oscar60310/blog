b632622825e1ae23e6393389ad7a7ec9f889712a
Ivan Tsai <9553914+oscar60310@users.noreply.github.com>
2022-10-13 23:34:14 +0800

    init docusaurus

--- themes/goofy/scripts/message.js
+++
@@ -1,9 +0,0 @@
-hexo.extend.tag.register("info", function(args) {
-  const message = args[0];
-  return `<div class="msg-info">${message}</div>`;
-});
-
-hexo.extend.tag.register("warn", function(args) {
-  const message = args[0];
-  return `<div class="msg-warn">${message}</div>`;
-});
